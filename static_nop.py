1d17ee13930d9e44bc490ba7b00f28ddaf1ac58a
Knightingale <kg@cyberknights.io>
2018-12-27 16:40:09 +0200

    Created a part1 branch

--- static_nop.py
+++ static_nop.py
@@ -1,39 +1,6 @@
 #!/usr/bin/python
-from elfhelpers import *
 from sys import argv
 from elftools.elf.elffile import ELFFile
-from capstone import *
-from capstone.arm import *
-
-def push_bin1(elf, binstr, offset):
-	# Now, get all the code sections (usually starts with .init and ands in .fini)
-	init = elf.get_section_by_name(".init")
-	code_start_off = init.header.sh_offset
-	
-	# Get the end offset
-	fini = elf.get_section_by_name(".fini")
-	code_end_off = fini.header.sh_offset + fini.header.sh_size
-	
-	print ".init start:", code_start_off, "X size:", code_end_off - code_start_off
-
-	# Set the file caret at beginning of code
-	elf.stream.seek(code_start_off)
-
-	# Read code
-	code = elf.stream.read(code_end_off - code_start_off)
-
-	# Get the .text section header as usually we'll inject there
-	text = elf.get_section_by_name(".text")
-		
-	# Remember offset from init	
-	off_from_init = text.header.sh_offset - init.header.sh_offset + offset
-
-	# Inject the code
-	code = code[:off_from_init] + binstr + code[off_from_init:]
-	
-	new_elf = fix_elf(elf, [(off_from_init, 2)], code)
-
-	return new_elf
 
 def push_bin(elf, binstr, offset):
 	# Get text information
